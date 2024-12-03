5c8f05283f346c785506e4fa17d0140dc78ae8f7
Urusha <urusha2517@gmail.com>
2024-12-03 15:04:03 -0500

    Inheritance and Object Type Casting.iml

---
+++ src/Girl.java
@@ -0,0 +1,16 @@
+public class Girl extends Person {
+    static double   ageFactor = 1.3;
+    public Girl(String aName) {
+        name = "Ms." + aName;
+    }
+
+    public String talk() {
+        return("Hello! " + jump());
+    }
+    public String jump() {
+        return("I am jumping.");
+    }
+    public static double lifeSpan() {
+        return(lifeSpan * ageFactor);
+    }
+}
